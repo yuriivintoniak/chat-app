36fc07602f48eb4c760fb37ac2232889057fcee8
yuriivintoniak <you@example.com>
2025-01-03 09:38:29 +0200

    refactor: move MongoDB connection logic to a separate file

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { connectToDatabase } = require("./db");
 
-require("dotenv").config();
-
+connectToDatabase();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
-
 app.use(cors());
 app.use(express.json());
-
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
-}
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
 app.listen(port, () => {
-  connectToDatabase();
   console.log(`Server is running on http://localhost:${port}`);
 });
